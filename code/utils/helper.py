bea613fb7dbd8ab9a784de17bc86290523a76b58
hugLiu <lliu606@hotmail.com>
2021-02-02 12:34:05 +0800

    Update helper.py

--- code/utils/helper.py
+++ code/utils/helper.py
@@ -11,6 +11,8 @@ def set_seed(seed):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
+    # 提升训练速度，训练结果保持一致
+    torch.backends.cudnn.benchmark = True
 
 def search_f1_auc(y_true, y_score):
     """
